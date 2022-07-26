3774e5c7d64c0ce26e7a3ce8cc7275a5ad1e8867
PaMarius <marius_maryus1997@yahoo.com>
2022-07-26 17:11:29 +0300

    implementing add points button

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-import {mockData} from "./mockData.js"
+import { mockData } from "./mockData.js";
 
-const body = document.querySelector('body')
-const cards = document.querySelector('.cards')
-
-// Driver's stats
-const driverStats = document.querySelector(".driver-stats");
-const rank = document.querySelector(".rank");
-const driverPoints = document.querySelector(".driver-points");
-
-// Driver's info
-const driverInfo = document.querySelector(".driver-info");
-const driverName = document.querySelector(".driver-name");
-const firstName = document.querySelector(".first-name");
-const lastName = document.querySelector(".last-name");
-const driverCountry = document.querySelector(".driver-country");
-
-// Driver's team
-const driverTeam = document.querySelector(".driver-team");
-
-// Driver's appearance
-const driverAppearance = document.querySelector(".driver-appearance");
-const driverNumber = document.querySelector(".driver-number");
-const driverPhoto = document.querySelector(".driver-photo");
+const body = document.querySelector("body");
+const cards = document.querySelector(".cards");
 
 // Change points button
 const button = document.querySelector(".button");
 const pointAdded = document.querySelector(".form__field");
-const selectedTeam = document.querySelector(".select-team")
+const selectedTeam = document.querySelector(".select-team");
 
-
-const sortedMockData = mockData.sort((a,b) => (a.points < b.points) ? 1 : ((b.points < a.points) ? -1 : 0));
-console.log(sortedMockData);
+const sortedMockData = mockData.sort((a, b) =>
+  a.points < b.points ? 1 : b.points < a.points ? -1 : 0
+);
 
 const createDrivers = sortedMockData.forEach((driver, index) => {
-    const {firstName, lastName, number, team, points, image, country, hex} = driver;
-let newDriver = document.createElement('div');
-newDriver.innerHTML = `<div class="driver-card">
+  const { firstName, lastName, number, team, points, image, country, hex } =
+    driver;
+  let newDriver = document.createElement("div");
+  newDriver.innerHTML = `<div class="driver-card">
 <div class="driver-stats">
-  <div class="rank">${index+1}</div>
+  <div class="rank">${index + 1}</div>
   <div class="driver-points">
   ${points} <span class="backg-blk points">PTS</span>
   </div>
@@ -67,15 +48,29 @@ newDriver.innerHTML = `<div class="driver-card">
 </div>
 </div>`;
 
-cards.appendChild(newDriver);
-}   
-);
+  cards.appendChild(newDriver);
+});
 
 let pointsToAdd;
 let teamToAddPoints;
+const teams = document.body.querySelectorAll(".driver-team");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   pointsToAdd = Number(pointAdded.value);
   teamToAddPoints = selectedTeam.value;
-  if (teamToAddPoints !== 'Select team:') {} else {alert("Please select a team!")}
-})
+  if (teamToAddPoints !== "Select team:") {
+  } else {
+    alert("Please select a team!");
+  }
+  const driversToAddPoints = sortedMockData.forEach((driver, index) => {
+    if (driver.team == teamToAddPoints) {
+      let driverPoints =
+        teams[index].parentElement.querySelector(".driver-points").firstChild
+          .textContent;
+      let sumOfPoints = Number(driverPoints) + pointsToAdd;
+      return (teams[index].parentElement.querySelector(
+        ".driver-points"
+      ).firstChild.textContent = sumOfPoints);
+    }
+  });
+});
